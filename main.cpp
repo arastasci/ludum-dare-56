cd626593e43c9027a83097eb3e67392dbfc7a46a
arastasci <arastasci@gmail.com>
2024-10-04 20:19:21 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,42 @@
+// ludum-dare-56.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <glad/glad.h>
+#include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <stb_image.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <vector> 
+int main()
+{
+    // Initialize GLFW
+    if (!glfwInit())
+    {
+		std::cout << "Failed to initialize GLFW\n";
+		return -1;
+	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* window = glfwCreateWindow(600, 400, "Minecraft alpha 1.0", NULL, NULL);
+
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwMakeContextCurrent(window);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glClearColor(0.69f, 0.92f, 0.92f, 1.0f);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
+	return 0;
+}
