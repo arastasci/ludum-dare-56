27c622b9303b36728eb1de022451f5c90c6e9c0c
arastasci <arastasci@gmail.com>
2024-10-07 01:22:33 +0300

    add finding gameobjects functionality

--- src/engine/base/Scene.h
+++ src/engine/base/Scene.h
@@ -2,7 +2,7 @@
 #include "../physics/collider.h"
 
 class GameObject;
-
+class Component;
 #define GAMEOBJECT_COUNT 256
 #define COLLIDER_COUNT 256
 
@@ -17,7 +17,38 @@ public:
 	std::vector<GameObject*> GameObjects;
 	std::vector<Collider*> Colliders;
 
+	template <typename T, typename = std::enable_if_t<std::is_base_of_v<GameObject, T>>>
+	T* FindGameObjectOfType()
+	{
+		for (auto g : GameObjects)
+		{
+			T* castedG = static_cast<T*>(g);
+			if (castedG != nullptr)
+				return castedG;
+		}
+		return nullptr;
+	}
+
+	template <typename T, typename = std::enable_if_t<std::is_base_of_v<Component, T>>>
+	T* FindComponentOfType()
+	{
+		for (auto g : GameObjects)
+		{
+			auto components = g->GetComponents();
+			for (int i = 0; i < components.size(); i++)
+			{
+				T* component = dynamic_cast<T*>(components[i]);
+
+				if (component)
+				{
+					return component;
+				}
+			}
+			
+		}
+		return nullptr;
+	}
+
 private:
 	std::vector<GameObject*> m_awaitingToStart;
-	void startGameObject(GameObject* go);
 };
--- src/game/scripts/GameManagerBehaviour.cpp
+++ src/game/scripts/GameManagerBehaviour.cpp
@@ -7,16 +7,32 @@ GameManagerBehaviour* GameManagerBehaviour::GetInstance()
 {
 	if (instance == nullptr)
 	{
-		instance = new GameManagerBehaviour();
+		auto b = Game::GetInstance()->ActiveScene->FindComponentOfType<GameManagerBehaviour>();
+		if (b == nullptr)
+		{
+			std::cout << "This shouldnt' happen."  << std::endl;
+			b = new GameManagerBehaviour();
+		}
+		instance = b;
 	}
 	return instance;
 }
 void GameManagerBehaviour::Start()
 {
+	for (int i = 0; i < m_lives; i++)
+	{
+		m_livesUI.push_back(GameObject::Instantiate<Life>({
+			{ 5.f + 4.0f * (i - 1) * 0.5f, 12.f, 0.0f },
+			{ 1, 1, 1},
+		{0, 0, 0} }
+		));
+
+	}
 }
 
 void GameManagerBehaviour::Awake()
 {
+	
 }
 
 void GameManagerBehaviour::IncreaseEnemy()
@@ -28,20 +44,21 @@ void GameManagerBehaviour::DecreaseEnemy()
 	m_enemyCount--;
 	if (m_enemyCount == 0)
 	{
-		EndGame(true);
+		endGame(true);
 	}
 }
 
 void GameManagerBehaviour::OnEnemyReachedTarget()
 {
 	m_lives--;
+	decreaseLife();
 	if (m_lives <= 0)
 	{
-		EndGame(false);
+		endGame(false);
 	}
 }
 
-void GameManagerBehaviour::EndGame(bool won)
+void GameManagerBehaviour::endGame(bool won)
 {
 	if (won)
 	{
@@ -50,6 +67,15 @@ void GameManagerBehaviour::EndGame(bool won)
 	else
 	{
 		std::cout << "You lost!" << std::endl;
+	}
+}
+
+void GameManagerBehaviour::decreaseLife()
+{
+	if (m_livesUI.size() > 0)
+	{
+		m_livesUI[m_livesUI.size() - 1]->Destroy();
+		m_livesUI.pop_back();
 	}
 }
 
